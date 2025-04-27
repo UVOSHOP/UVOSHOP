6b943405ee2cd17bf54fed989bd9280c8a56bc1f
UVOSHOP <adit.gam775@gmail.com>
2025-04-27 23:30:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Fungsi untuk mengarahkan ke WhatsApp sesuai game yang dipilih
 function redirectToWA(game) {
     const message = `Halo, saya ingin membeli produk untuk game ${game}.`;
     const waUrl = `https://wa.me/6285648211278?text=${encodeURIComponent(message)}`;
