2b0c079aa57708f3c94b002126ae9d231a94baa0
UVOSHOP <adit.gam775@gmail.com>
2025-04-27 23:23:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-// Fungsi untuk mengarahkan ke WhatsApp sesuai game yang dipilih
-function redirectToWA(gameName) {
-    const phoneNumber = "6285648211278";  // Ganti dengan nomor WhatsApp yang sesuai
-    const message = Halo, saya ingin melakukan top-up untuk game ${gameName}. Mohon bantuannya.;
-
-    const encodedMessage = encodeURIComponent(message);
-    const url = https://wa.me/${phoneNumber}?text=${encodedMessage};
-
-    window.open(url, '_blank');  // Membuka WhatsApp di tab baru
+function redirectToWA(game) {
+    const message = `Halo, saya ingin membeli produk untuk game ${game}.`;
+    const waUrl = `https://wa.me/6285648211278?text=${encodeURIComponent(message)}`;
+    window.open(waUrl, '_blank');
 }
