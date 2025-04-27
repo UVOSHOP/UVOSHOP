8db1bdcd641ac4ded5c6b8ec76cbc1425b671d2d
UVOSHOP <adit.gam775@gmail.com>
2025-04-27 21:27:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-// Inisialisasi Swiper untuk kategori
-new Swiper('.kategoriSwiper', {
-    slidesPerView: 'auto',
-    spaceBetween: 10,
-    freeMode: true,
-});
-
-// Inisialisasi Swiper untuk game (kalau mau geser horizontal juga)
-new Swiper('.gameSwiper', {
-    slidesPerView: 'auto',
-    spaceBetween: 15,
-    freeMode: true,
-});
-
-// Daftar game
 const games = [
+    // Mobile Games
     { name: "Mobile Legends", img: "https://play-lh.googleusercontent.com/yzXkSh1_A_MRNLi3WJ2jAp7u5f50RHIUf3Jr0gFzLDb2s5CB_JYxv-OqWID1rZmYHGI=w240-h480", kategori: "Mobile" },
     { name: "PUBG Mobile", img: "https://play-lh.googleusercontent.com/IC9DXDiJD51rWGDLXwj-CWneP4Crg2M7tCGC0dXWyrDFRG8svZXkIHkXAQ7R9wK6Vg=w240-h480", kategori: "Mobile" },
     { name: "Genshin Impact", img: "https://play-lh.googleusercontent.com/5f3XZtrWJzW2NUKIpG5bUGD1bTW2O4bwkwN_9X-gkWfIPkSgTzNix8hYt_K0S8SByBc=w240-h480", kategori: "Mobile" },
-    { name: "Valorant", img: "https://playvalorant.com/assets/press-kit/VALORANT-Key-Art.jpg", kategori: "PC" },
-    { name: "Point Blank", img: "https://cdn.unipin.com/images/merchant/logo/point-blank-new.jpg", kategori: "PC" },
     { name: "Free Fire", img: "https://play-lh.googleusercontent.com/GI-yE8UMNC-bgiqEavukTYHTfPYf_pHC0mrGkMyzPqDMEv1lmX5pyY0I0ixTKNccLQ=w240-h480", kategori: "Mobile" },
     { name: "Call of Duty Mobile", img: "https://play-lh.googleusercontent.com/SzP6pNEqIA7s4B1uHK5aRzLG8d_xWv0q4nq0hYp2e4WIBW7bgvjAuG_0hAdfAQV3WQo=w240-h480", kategori: "Mobile" },
     { name: "Arena of Valor", img: "https://play-lh.googleusercontent.com/cp8bGwOPzDKP_9RX37vtZ4FbFCI4TMEgx_ZkwSihcsCwSasq9VWhK_xOAMzVJQJ7Kw=w240-h480", kategori: "Mobile" },
     { name: "Honkai Impact", img: "https://play-lh.googleusercontent.com/IqkK1wj4vX7sT5w2XhKeb7gyZKPyPTD91n3J0zDZffM7bNlBJRI5rPrUdK4-8M3h0mw=w240-h480", kategori: "Mobile" },
     { name: "Ragnarok X", img: "https://play-lh.googleusercontent.com/VhYYawXxKDbpoevKyNhrguJdFOQnFrUKWBomzZ8UdBQHnLZP8SsqHvBrXcVO0k09hr0=w240-h480", kategori: "Mobile" },
-    // Tambahkan lagi game lainnya kalau mau total 20-30 ya!
+    { name: "Clash of Clans", img: "https://play-lh.googleusercontent.com/MiNOwSeDjXYeM7yFxoTXR2ZGrrdC0gYwFQTwjlK1VpHcfFC7LO7FC9lWqxEdF3Lr9E=w240-h480", kategori: "Mobile" },
+    { name: "Brawl Stars", img: "https://play-lh.googleusercontent.com/K1FVQAmCmDjmZ6vS4cZ9TSmg7A7hde_fDRrUwV7rTnH3LVZ7tHJ8YVVY3imZzYv0-K0=w240-h480", kategori: "Mobile" },
+    { name: "Clash Royale", img: "https://play-lh.googleusercontent.com/h68y1UlMlJ6mrzF8dxVw70dCR38dbvXNKAZ39N-6M-D0flF53KPyLs5w7xzds_dqfFq=w240-h480", kategori: "Mobile" },
+
+    // PC Games
+    { name: "Valorant", img: "https://playvalorant.com/assets/press-kit/VALORANT-Key-Art.jpg", kategori: "PC" },
+    { name: "Point Blank", img: "https://cdn.unipin.com/images/merchant/logo/point-blank-new.jpg", kategori: "PC" },
+    { name: "League of Legends", img: "https://upload.wikimedia.org/wikipedia/commons/9/9f/League_of_Legends_logo_2019.svg", kategori: "PC" },
+    { name: "Dota 2", img: "https://upload.wikimedia.org/wikipedia/commons/a/a7/Dota_2_logo.svg", kategori: "PC" },
+    { name: "Counter-Strike: Global Offensive", img: "https://upload.wikimedia.org/wikipedia/commons/f/f7/CSGO_Logo.png", kategori: "PC" },
+    { name: "Fortnite", img: "https://upload.wikimedia.org/wikipedia/commons/3/37/Fortnite_logo.svg", kategori: "PC" },
+    { name: "Apex Legends", img: "https://upload.wikimedia.org/wikipedia/commons/3/36/Apex_Legends_logo.svg", kategori: "PC" },
+    { name: "Overwatch 2", img: "https://upload.wikimedia.org/wikipedia/commons/d/d7/Overwatch_2_logo.png", kategori: "PC" },
+    { name: "Warframe", img: "https://upload.wikimedia.org/wikipedia/commons/2/27/Warframe_logo_2018.svg", kategori: "PC" },
+    { name: "Minecraft", img: "https://upload.wikimedia.org/wikipedia/commons/5/51/Minecraft_logo.png", kategori: "PC" },
+
+    // Voucher Games
+    { name: "Steam", img: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Steam_logo.png", kategori: "Voucher" },
+    { name: "PlayStation Network", img: "https://upload.wikimedia.org/wikipedia/commons/5/5f/PlayStation_Logo.png", kategori: "Voucher" },
+    { name: "Xbox Live", img: "https://upload.wikimedia.org/wikipedia/commons/2/2d/Xbox_logo_%282016%29.svg", kategori: "Voucher" },
+    { name: "Google Play Gift Cards", img: "https://upload.wikimedia.org/wikipedia/commons/0/02/Google_Play_Store_Logo_2019.png", kategori: "Voucher" },
+    { name: "Apple iTunes Gift Cards", img: "https://upload.wikimedia.org/wikipedia/commons/4/42/Apple_iTunes_logo.svg", kategori: "Voucher" },
+    { name: "Amazon Gift Cards", img: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg", kategori: "Voucher" },
+    { name: "Nintendo eShop", img: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Nintendo_eShop_logo.png", kategori: "Voucher" },
+    { name: "Riot Points (RP)", img: "https://upload.wikimedia.org/wikipedia/commons/7/75/Riot_Games_logo_2020.png", kategori: "Voucher" },
+    { name: "Garena Shells", img: "https://upload.wikimedia.org/wikipedia/commons/6/69/Garena_logo.png", kategori: "Voucher" },
+    { name: "Facebook Credits", img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Facebook_logo_2023.png", kategori: "Voucher" }
 ];
 
-// Render game
+// Render Game Function
 const gameList = document.getElementById('game-list');
 games.forEach(game => {
     const gameItem = document.createElement('div');
